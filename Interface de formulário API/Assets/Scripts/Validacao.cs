2f4f3a51e6f2785a977146441962aafe0cd53364
Bruno Gomes <brunogomess7@gmail.com>
2018-02-07 09:53:02 -0300

    Interface formulário

---
+++ Interface de formulário API/Assets/Scripts/Validacao.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Validacao : MonoBehaviour {
+
+	[SerializeField]
+	private InputField campoFisioterapeuta;
+	[SerializeField]
+	private InputField campoPaciente;
+	[SerializeField]
+	private Text aviso = null;
+	[SerializeField]
+	private Button avancar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void validar () {
+		string fisioterapeuta = campoFisioterapeuta.text;
+		string paciente = campoPaciente.text;
+
+		if (fisioterapeuta == "" || paciente == "") {
+			aviso.CrossFadeAlpha (100f, 1f, false);
+			aviso.color = Color.red;
+			aviso.text = "Os campos acima são obrigatórios.";
+			aviso.CrossFadeAlpha (0f, 2f, false);
+		} else {
+			Application.LoadLevel ("Parametros");
+		}
+	}
+
+	public void configuracao(){
+		Application.LoadLevel ("Configuracoes");
+	}
+
+	public void voltar(){
+		Application.LoadLevel ("Paciente");
+	}
+}
